aa804f2ea1f7d574d5fb75bcc99f07edff1fe5c6
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2022-04-15 13:51:24 +0200

    Création des cartes animées et flips

--- douce/src/components/Menu/Corps.js
+++ douce/src/components/Menu/Corps.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import './tarif.css'
+import ReactCardFlip from 'react-card-flip';
 
 function Corps() {
+
+    const [isFlippedFirst, setIsFlippedFirst] = useState(false);
+    const [isFlippedSecond, setIsFlippedSecond] = useState(false);
+    const [isFlippedThird, setIsFlippedThird] = useState(false);
+    const [isFlippedFourth, setIsFlippedFourth] = useState(false);
 
     const [epilation, setEpilation] = useState([])
     const [corps, setCorps] = useState([])
@@ -10,6 +16,24 @@ function Corps() {
     const [mains, setMains] = useState([])
     const [pieds, setPieds] = useState([])
     const [makeup, setMakeup] = useState([])
+
+    const handleClickFirst = (e) => {
+        e.preventDefault();
+        setIsFlippedFirst(!isFlippedFirst);
+    };
+    const handleClickSecond = (e) => {
+        e.preventDefault();
+        setIsFlippedSecond(!isFlippedSecond);
+    };
+    const handleClickThird = (e) => {
+        e.preventDefault();
+        setIsFlippedThird(!isFlippedThird);
+    };
+    const handleClickFourth = (e) => {
+        e.preventDefault();
+        setIsFlippedFourth(!isFlippedFourth);
+    };
+
 
     useEffect(() => {
         axios.get('http://localhost:8080/api/menu/epilation')
@@ -48,6 +72,172 @@ function Corps() {
     return (
         < >
             <div className="cards tarif-container-center">
+                <ReactCardFlip isFlipped={isFlippedFirst} flipDirection="horizontal">
+                    <div onClick={handleClickFirst} className='front-card'>
+                        <div className="card-body">
+                            <div className='front-card-style'>
+                                {/* <div className='bubble'>
+                                </div> */}
+                                <div className='blob-container'>
+                                    <div class="blob2">
+
+                                        <svg src="http://www.w3.org/1999/xlink" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 310 350">
+                                            <path d="M156.4,339.5c31.8-2.5,59.4-26.8,80.2-48.5c28.3-29.5,40.5-47,56.1-85.1c14-34.3,20.7-75.6,2.3-111  c-18.1-34.8-55.7-58-90.4-72.3c-11.7-4.8-24.1-8.8-36.8-11.5l-0.9-0.9l-0.6,0.6c-27.7-5.8-56.6-6-82.4,3c-38.8,13.6-64,48.8-66.8,90.3c-3,43.9,17.8,88.3,33.7,128.8c5.3,13.5,10.4,27.1,14.9,40.9C77.5,309.9,111,343,156.4,339.5z" />
+                                        </svg>
+
+                                    </div>
+
+                                <div className='front-card-text-container'>
+                                    <div className='front-card-title'>
+                                    <h1>Epilation</h1>
+                                    </div>
+                                    <div className='front-card-text'>
+                                    <p>Cliquer pour affichier</p>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div onClick={handleClickFirst} className='back-card'>
+                        <div className="card-body">
+                            {epilation.map(item => (
+                                <div className="card-menu-container" style={{ width: '18rem' }}>
+                                    <h5 className="card-title">{item.bis === null ? item.nom + '' : item.nom + ' ' + item.bis} </h5>
+                                    <p className="card-price">{item.prix + '€'}</p>
+                                </div>
+                            ))}
+                        </div>
+
+                    </div>
+                </ReactCardFlip>
+                <ReactCardFlip isFlipped={isFlippedSecond} flipDirection="horizontal">
+                    <div onClick={handleClickSecond} className='front-card'>
+                    <div className="card-body">
+                            <div className='front-card-style'>
+                                {/* <div className='bubble'>
+                                </div> */}
+                                <div className='blob-container'>
+                                    <div class="blob2">
+
+                                        <svg src="http://www.w3.org/1999/xlink" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 310 350">
+                                            <path d="M156.4,339.5c31.8-2.5,59.4-26.8,80.2-48.5c28.3-29.5,40.5-47,56.1-85.1c14-34.3,20.7-75.6,2.3-111  c-18.1-34.8-55.7-58-90.4-72.3c-11.7-4.8-24.1-8.8-36.8-11.5l-0.9-0.9l-0.6,0.6c-27.7-5.8-56.6-6-82.4,3c-38.8,13.6-64,48.8-66.8,90.3c-3,43.9,17.8,88.3,33.7,128.8c5.3,13.5,10.4,27.1,14.9,40.9C77.5,309.9,111,343,156.4,339.5z" />
+                                        </svg>
+
+                                    </div>
+
+                                <div className='front-card-text-container'>
+                                    <div className='front-card-title'>
+                                    <h1>Soins Corps</h1>
+                                    </div>
+                                    <div className='front-card-text'>
+                                    <p>Cliquer pour affichier</p>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div onClick={handleClickSecond} className='back-card'>
+                        <div className="card-body">
+                            {corps.map(item => (
+                                <div className="card-menu-container" style={{ width: '18rem' }}>
+                                    <h5 className="card-title">{item.bis === null ? item.nom + '' : item.nom + ' ' + item.bis} </h5>
+                                    <p className="card-price">{item.prix + '€'}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </ReactCardFlip>
+                <ReactCardFlip isFlipped={isFlippedThird} flipDirection="horizontal">
+                    <div onClick={handleClickThird} className='front-card'>
+                    <div className="card-body">
+                            <div className='front-card-style'>
+                                {/* <div className='bubble'>
+                                </div> */}
+                                <div className='blob-container'>
+                                    <div class="blob2">
+
+                                        <svg src="http://www.w3.org/1999/xlink" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 310 350">
+                                            <path d="M156.4,339.5c31.8-2.5,59.4-26.8,80.2-48.5c28.3-29.5,40.5-47,56.1-85.1c14-34.3,20.7-75.6,2.3-111  c-18.1-34.8-55.7-58-90.4-72.3c-11.7-4.8-24.1-8.8-36.8-11.5l-0.9-0.9l-0.6,0.6c-27.7-5.8-56.6-6-82.4,3c-38.8,13.6-64,48.8-66.8,90.3c-3,43.9,17.8,88.3,33.7,128.8c5.3,13.5,10.4,27.1,14.9,40.9C77.5,309.9,111,343,156.4,339.5z" />
+                                        </svg>
+
+                                    </div>
+
+                                <div className='front-card-text-container'>
+                                    <div className='front-card-title'>
+                                    <h1>Soins Visage</h1>
+                                    </div>
+                                    <div className='front-card-text'>
+                                    <p>Cliquer pour affichier</p>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div onClick={handleClickThird} className='back-card'>
+                        <div className="card-body">
+                            {epilation.map(item => (
+                                <div className="card-menu-container" style={{ width: '18rem' }}>
+                                    <h5 className="card-title">{item.bis === null ? item.nom + '' : item.nom + ' ' + item.bis} </h5>
+                                    <p className="card-price">{item.prix + '€'}</p>
+                                </div>
+                            ))}
+                        </div>
+
+                    </div>
+                </ReactCardFlip>
+                <ReactCardFlip isFlipped={isFlippedFourth} flipDirection="horizontal">
+                    <div onClick={handleClickFourth} className='front-card'>
+                    <div className="card-body">
+                            <div className='front-card-style'>
+                                {/* <div className='bubble'>
+                                </div> */}
+                                <div className='blob-container'>
+                                    <div class="blob2">
+
+                                        <svg src="http://www.w3.org/1999/xlink" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 310 350">
+                                            <path d="M156.4,339.5c31.8-2.5,59.4-26.8,80.2-48.5c28.3-29.5,40.5-47,56.1-85.1c14-34.3,20.7-75.6,2.3-111  c-18.1-34.8-55.7-58-90.4-72.3c-11.7-4.8-24.1-8.8-36.8-11.5l-0.9-0.9l-0.6,0.6c-27.7-5.8-56.6-6-82.4,3c-38.8,13.6-64,48.8-66.8,90.3c-3,43.9,17.8,88.3,33.7,128.8c5.3,13.5,10.4,27.1,14.9,40.9C77.5,309.9,111,343,156.4,339.5z" />
+                                        </svg>
+
+                                    </div>
+
+                                <div className='front-card-text-container'>
+                                    <div className='front-card-title'>
+                                    <h1>Mains & Pieds</h1>
+                                    </div>
+                                    <div className='front-card-text'>
+                                    <p>Cliquer pour affichier</p>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div onClick={handleClickFourth} className='back-card'>
+                        <div className="card-body">
+                            {epilation.map(item => (
+                                <div className="card-menu-container" style={{ width: '18rem' }}>
+                                    <h5 className="card-title">{item.bis === null ? item.nom + '' : item.nom + ' ' + item.bis} </h5>
+                                    <p className="card-price">{item.prix + '€'}</p>
+                                </div>
+                            ))}
+                        </div>
+
+                    </div>
+                </ReactCardFlip>
+            </div>
+
+            {/* <div className="cards tarif-container-center">
                 <div className='big-card-container'>
                     <div className="card-body">
                         {epilation.map(item => (
@@ -108,7 +298,7 @@ function Corps() {
                     <div className="card-body">
                     </div>
                 </div>
-            </div>
+            </div> */}
         </>
     )
 }
